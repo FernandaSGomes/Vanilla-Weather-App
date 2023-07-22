232751dfe2bf6433ae9108e72a1d289ea382ecf7
FernandaSGomes <nandasthefany@hotmail.com>
2023-07-22 02:03:36 +0100

    Updated the css

--- src/app.js
+++ src/app.js
@@ -63,7 +63,6 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
